19880e8426c18b47941490f9784beee2def00a69
Akash <akashkopparthi@gmail.com>
2026-04-28 12:40:38 +0530

    Added new file

---
+++ ASCII.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ASCII{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        char str=s.next().charAt(0);
+        int ascii=str;
+        System.out.println(ascii);
+    }
+}
